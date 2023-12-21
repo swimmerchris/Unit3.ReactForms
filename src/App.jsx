d0209359d2815b4f74a307b14e249d74444a1fb5
Christopher Bryan <swimmer.chris@gmail.com>
2023-12-20 21:39:56 -0500

    updating to the current solution up to this point

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import './App.css'
+// components
+import SignUpForm from './components/SignUpForm'
+import Authenticate from './components/Authenticate'
+
+
 
 function App() {
 
   return (
     <>
+    <Authenticate />
+    <SignUpForm/>
     </>
   )
 }
